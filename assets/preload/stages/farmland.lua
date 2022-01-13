467025431cbe7fabfb8dc1c063d2a988329c8605
Poky211 <97640379+Poky211@users.noreply.github.com>
2022-01-13 14:44:20 -0300

    Add files via upload

---
+++ assets/preload/stages/farmland.lua
@@ -0,0 +1,8 @@
+local angleshit = 1;
+local anglevar = 1;
+function onCreate()
+	makeLuaSprite('bg','farmland',-300, -100)
+	addLuaSprite('bg',false)
+	
+end
+
---
+++ assets/preload/stages/hills.lua
@@ -0,0 +1,7 @@
+local angleshit = 1;
+local anglevar = 1;
+function onCreate()
+	makeLuaSprite('bg','hills',-300, -100)
+	addLuaSprite('bg',false)
+	
+end
